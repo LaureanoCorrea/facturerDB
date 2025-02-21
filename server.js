72a4524b898f19d60a5c4575ea5f48c7ba9257ce
laureano correa <loriensdesign@gmail.com>
2025-02-21 19:34:18 -0300

    index

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/auth');
+const mainRoutes = require('./routes'); // Importa el index.js de rutas
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,9 +16,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Montar el router. Si usas un prefijo, la URL cambiará.
+// Montar las rutas
 app.use('/api', authRoutes);
+app.use('/', mainRoutes); // Agrega esta línea
 
+// Iniciar el servidor
 app.listen(PORT, () => {
 	console.log(`🔥 Servidor corriendo en http://localhost:${PORT}`);
 });
