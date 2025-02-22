60d4d573d3c05f1b3908066589aa62b14c73d0f2
laureano correa <loriensdesign@gmail.com>
2025-02-22 12:00:17 -0300

    arreglo de model

--- models/user.model.js
+++ models/user.model.js
@@ -1,13 +1,17 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ["director", "admin"], default: "director" },
-  logoUrl: { type: String, default: null },
-  teamName: { type: String, default: ""}, 
-  alias: {type: String, default:""}
+	username: { type: String, required: true, unique: true },
+	password: { type: String, required: true },
+	role: {
+		type: String,
+		enum: ['director', 'admin', 'coordinador'],
+		default: 'coordinador',
+	},
+	logoUrl: { type: String, default: null },
+	teamName: { type: String, default: '' },
+	alias: { type: String, default: '' },
 });
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model('User', UserSchema);
